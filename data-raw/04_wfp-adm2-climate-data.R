bda003d79ba566d6d442a34a1a4bf4181d222673
Zack <zackarno@gmail.com>
2025-01-31 17:05:37 -0500

    get chirps from wfp rather than static gee file for easier updating

--- data-raw/04_wfp-adm2-climate-data.R
+++ data-raw/04_wfp-adm2-climate-data.R
@@ -6,12 +6,18 @@
 #' Nonetheless the files are being stored in the blob in case of any future
 #' changes to links and to make qmd -> html rendering faster
 
-box::use(purrr[map2])
-box::use(readr[write_csv])
-box::use(AzureStor)
 
-box::use(../R/load_funcs[...])
-box::use(blob = ../R/blob_connect[...])
+box::use(
+  AzureStor,
+  readr[write_csv],
+  purrr[map2]
+  )
+
+box::use(
+  ../R/load_funcs[...],
+  blob = ../R/blob_connect[...]
+)
+
 
 pc <- blob$load_proj_containers()
 fps <- blob$proj_blob_paths()
@@ -34,3 +40,50 @@ map2(fp_names, list(df_chirps_adm2,df_ndvi_adm2),
       )
     }
 )
+
+
+box::use(
+  ../R/load_funcs
+)
+box::reload(load_funcs)
+df_adm2_chirps <- load_funcs$load_wfp_chirps()
+
+df_adm2_yr_mo <- df_adm2_chirps |>
+  group_by(
+    yr_mo_date = floor_date(date,"month"),
+    adm2_id,
+    adm2_pcode
+  ) |>
+  summarise(
+    precipitation = sum(rfh),
+    n_pixels = unique(n_pixels)
+  ) |>
+  ungroup()
+df_lookup <- cumulus$blob_load_admin_lookup()
+
+df_chirps_adm1 <- df_lookup |>
+  clean_names() |>
+  filter(adm_level ==2) |>
+  right_join(df_adm2_yr_mo) |>
+  group_by(date=yr_mo_date, adm1_name,adm1_pcode) |>
+  summarise(
+    value = weighted.mean(precipitation,n_pixels),
+    .groups= "drop"
+  ) |>
+  mutate(
+    parameter = "CHIRPS (WFP)"
+  )
+
+cumulus$blob_write(df_chirps_adm1,name= fps$DF_ADM2_CHIRPS_WFP, stage="dev")
+
+
+# df_chirps_gee <- load_funcs$load_chirps()
+# df_chirps_gee |>
+#   clean_names() |>
+#   select(adm1_code,adm1_name,date,value) |>
+#   right_join(
+#     df_chirps_adm1, by= c("adm1_name","date")
+#   ) |>
+#   pull(date) |>
+#   range()
+#   range(date)
